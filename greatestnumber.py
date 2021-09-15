5088116797acf1793f460ea49330c8a164a38e5a
Bboynegi <90628717+Bboynegi@users.noreply.github.com>
2021-09-15 19:58:29 +0530

    Add files via upload

---
+++ greatestnumber.py
@@ -0,0 +1,9 @@
+a = int(input("enter number a:"))
+b = int(input("enter number b:"))
+c = int(input("enter number c:"))
+if a > b and a > c:
+    print("a is the greaatest number")
+elif b > c and b > a:
+    print("b is the greatest number")
+else:
+    print("c is the greatest number") 
